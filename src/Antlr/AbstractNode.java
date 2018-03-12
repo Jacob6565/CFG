a6f4427c536496ef74781def980c5e2c1d629806
Magnus Winkel Pedersen <mwpe16@student.aau.dk>
2018-03-12 12:01:07 +0100

    Jeg har lavet et par klasser

---
+++ src/Antlr/AbstractNode.java
@@ -0,0 +1,39 @@
+package Antlr;
+
+abstract class AbstractNode
+{
+
+}
+
+abstract class ExpressionNode extends AbstractNode
+{
+
+}
+
+abstract class StatementNode extends  AbstractNode{
+
+}
+
+abstract class FunctionDeclarationNode extends AbstractNode{
+
+}
+
+abstract class StrategyDeclarationNode extends AbstractNode{
+
+}
+
+abstract class DeclarationNode extends AbstractNode{
+
+}
+
+abstract class ConditionDeclarationNode extends AbstractNode{
+
+}
+
+abstract class ControlStructureNode extends AbstractNode{
+
+}
+
+abstract class InfixExpressionNode extends ExpressionNode{
+
+}
