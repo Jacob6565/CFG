d407c123fd6c9af9ed283958b35f238558821284
JG <jsande16@student.aau.dk>
2018-03-13 10:47:13 +0100

    Tilføjede til AbstractNode

--- src/Antlr/ASTVisitor.java
+++ src/Antlr/ASTVisitor.java
@@ -1,6 +1,8 @@
 package Antlr;
 
-public class ASTVisitor
+public abstract class ASTVisitor<T>
 {
     //Har en overloaded visitmetoder for hver node.
+    public abstract T visit(AdditionNode node);
+    //Osv.
 }
