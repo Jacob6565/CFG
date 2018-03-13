f727b563f53a4180f50a9140d31ba46abb609585
Magnus Winkel Pedersen <mwpe16@student.aau.dk>
2018-03-13 09:37:43 +0100

    Endnu flere klasser

--- src/Antlr/AbstractNode.java
+++ src/Antlr/AbstractNode.java
@@ -1,4 +1,7 @@
 package Antlr;
+
+import java.beans.Statement;
+
 enum Color{
     red,
     blue,
@@ -42,17 +45,38 @@ abstract class ControlStructureNode extends AbstractNode{
 }
 
 abstract class InfixExpressionNode extends ExpressionNode{
+    public ExpressionNode LeftChild;
+    public ExpressionNode RightChild;
+}
+
+
+
+
+
+
+
+class IfControlStructureNode extends ControlStructureNode{
 
 }
 
-class IfControlStructureNode extends ControlStructureNode{
+class WhileNode extends ControlStructureNode{
+
+}
+
+class ForNode extends  ControlStructureNode{
+
+}
+
+class RoutineNode extends ControlStructureNode{
 
 }
 
 class RobotPropertiesNode extends AbstractNode{
     public TextNode Name;
     public TextNode RobotType;
-
+    public GunColorNode GunColor;
+    public BodyColorNode BodyColor;
+    public RadarColorNode RadarColor;
 }
 
 class GunColorNode extends AbstractNode{
@@ -75,6 +99,70 @@ class ColorValNode extends AbstractNode{
     public Color Color;
 }
 
-class NumberNode extends AbstractNode{
+class NumberNode extends ExpressionNode{
     public double Value;
+}
+
+class BoolNode extends ExpressionNode{
+
+}
+
+class AdditionNode extends InfixExpressionNode{
+
+}
+
+class SubtractionNode extends InfixExpressionNode{
+
+}
+
+class MultiplicationNode extends InfixExpressionNode{
+
+}
+
+class DivisionNode extends InfixExpressionNode{
+
+}
+
+class PowerNode extends InfixExpressionNode{
+    /* ex 3^4 */
+}
+
+class NotNode extends ExpressionNode{
+    public ExpressionNode Expression;
+}
+
+class AndNode extends InfixExpressionNode{
+
+}
+
+class OrNode extends InfixExpressionNode{
+
+}
+
+class ModuloNode extends InfixExpressionNode{
+
+}
+
+class AssignNode extends StatementNode{
+
+}
+
+class FuncCallNode extends StatementNode{
+
+}
+
+class ReturnNode extends StatementNode{
+
+}
+
+class NumberDeclarationNode extends DeclarationNode{ //Jeg ved ikke om vi skal lave typechecking endnu
+
+}
+
+class TextDeclarationNode extends DeclarationNode{
+
+}
+
+class BooleanDeclarationNode extends DeclarationNode{
+
 }
