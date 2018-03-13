5a4295c15ff6e2cbf25eb2985a3bc62d817d1628
Magnus Winkel Pedersen <mwpe16@student.aau.dk>
2018-03-13 09:14:37 +0100

    Jeg har lavet flere klasser nu

--- src/Antlr/AbstractNode.java
+++ src/Antlr/AbstractNode.java
@@ -1,5 +1,12 @@
 package Antlr;
-
+enum Color{
+    red,
+    blue,
+    yellow,
+    green,
+    black,
+    white
+}
 abstract class AbstractNode
 {
 
@@ -36,4 +43,38 @@ abstract class ControlStructureNode extends AbstractNode{
 
 abstract class InfixExpressionNode extends ExpressionNode{
 
+}
+
+class IfControlStructureNode extends ControlStructureNode{
+
+}
+
+class RobotPropertiesNode extends AbstractNode{
+    public TextNode Name;
+    public TextNode RobotType;
+
+}
+
+class GunColorNode extends AbstractNode{
+    public ColorValNode Color;
+}
+
+class BodyColorNode extends AbstractNode{
+    public ColorValNode Color;
+}
+
+class RadarColorNode extends AbstractNode{
+    public ColorValNode Color;
+}
+
+class TextNode extends AbstractNode{
+    public String Content;
+}
+
+class ColorValNode extends AbstractNode{
+    public Color Color;
+}
+
+class NumberNode extends AbstractNode{
+    public double Value;
 }
