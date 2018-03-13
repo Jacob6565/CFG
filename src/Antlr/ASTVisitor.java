1c359a6b5a87a8d5aa753897f959996f38fd8552
JG <jsande16@student.aau.dk>
2018-03-13 11:06:52 +0100

    Tilføjede til AbstractNode

--- src/Antlr/ASTVisitor.java
+++ src/Antlr/ASTVisitor.java
@@ -3,6 +3,50 @@ package Antlr;
 public abstract class ASTVisitor<T>
 {
     //Har en overloaded visitmetoder for hver node.
+    //Dette er lavet på baggrund af slide 82 lecture 9
     public abstract T visit(AdditionNode node);
-    //Osv.
+    public abstract T visit(AndNode node);
+    public abstract T visit(AssignNode node);
+    public abstract T visit(BodyColorNode node);
+    public abstract T visit(BooleanDeclarationNode node);
+    public abstract T visit(BoolNode node);
+    public abstract T visit(Color node);
+    public abstract T visit(ColorValNode node);
+    public abstract T visit(ConditionDeclarationNode node);
+    public abstract T visit(ControlStructureNode node);
+    public abstract T visit(DeclarationNode node);
+    public abstract T visit(DivisionNode node);
+    public abstract T visit(ForNode node);
+    public abstract T visit(FunctionDeclarationNode node);
+    public abstract T visit(GunColorNode node);
+    public abstract T visit(IdNode node);
+    public abstract T visit(IfControlStructureNode node);
+    public abstract T visit(InfixExpressionNode node);
+    public abstract T visit(ModuloNode node);
+    public abstract T visit(MultiplicationNode node);
+    public abstract T visit(NegateNode node);
+    public abstract T visit(NotNode node);
+    public abstract T visit(NumberDeclarationNode node);
+    public abstract T visit(NumberNode node);
+    public abstract T visit(OrNode node);
+    public abstract T visit(PowerNode node);
+    public abstract T visit(RadarColorNode node);
+    public abstract T visit(ReturnNode node);
+    public abstract T visit(RobotPropertiesNode node);
+    public abstract T visit(RoutineNode node);
+    public abstract T visit(StatementNode node);
+    public abstract T visit(StrategyDeclarationNode node);
+    public abstract T visit(TextDeclarationNode node);
+    public abstract T visit(TextNode node);
+    public abstract T visit(TypeFuncCallNode node);
+    public abstract T visit(VoidFuncCallNode node);
+    public abstract T visit(WhenNode node);
+    public abstract T visit(WhileNode node);
+
+    //Det er så disse som skal have if/else eller switched ud fra duden på stackoverflow.
+    public abstract T visit(AbstractNode node);
+    public abstract T visit(ExpressionNode node);
+
+
+
 }
